d02ca1eba8d7d4cb7014e8a685ad137c27464efe
ohjaiwoo <ojw7336@cau.ac.kr>
2025-10-28 21:28:33 +0900

    reportController class insert getReportsByAnalystsId

--- src/main/java/capstone25_2/aim/AimApplication.java
+++ src/main/java/capstone25_2/aim/AimApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AimApplication {
+public class  AimApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AimApplication.class, args);
--- src/main/java/capstone25_2/aim/controller/ReportController.java
+++ src/main/java/capstone25_2/aim/controller/ReportController.java
@@ -22,6 +22,14 @@ public class ReportController {
     private final ReportService reportService;
     private final AnalysisService analysisService;
 
+    // 특정 애널리스트의 리포트 리스트 조회
+    @GetMapping("/analyst/{analystId}")
+    public List<ReportResponseDTO> getReportsByAnalystId(@PathVariable Long analystId) {
+        return reportService.getReportsByAnalystId(analystId).stream()
+                .map(ReportResponseDTO::fromEntity)
+                .toList();
+    }
+
     // 특정 종목의 리포트 리스트 조회
     @GetMapping("/stock/{stockId}")
     public List<ReportResponseDTO> getReportsByStockId(@PathVariable Long stockId) {
